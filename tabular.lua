27829429436e6bbea7ff169742e2b255acc9c0f4
luk <mail-login+gitlab@protonmail.com>
2021-11-11 01:17:42 +0100

    initial commit (2)

---
+++ tabular.lua
@@ -0,0 +1,39 @@
+local function max(a,b) return (a ~= nil and a > b) and a or b end
+
+local function tabular_printing(content)
+	local colSizes = {}
+	for _,v1 in ipairs(content) do
+		for i2,v2 in ipairs(v1) do
+			colSizes[i2] = max(colSizes[i2], #v2)
+		end
+	end
+	local format = "| "
+	for _,v in ipairs(colSizes) do
+		local x
+		if v < 100 then x = v else
+			print("Warning one column is larger than 100 characters")
+			x = 99
+		end
+		format = format .. "%-" .. tostring(x) .. "s | "
+	end
+	local first = true
+	local function _sum(t)
+		local r = 0
+		for _,v in ipairs(t) do
+			r = r + v+3
+		end
+		return r
+	end
+	for _,v in ipairs(content) do
+		print(string.format(format, table.unpack(v, 1, #colSizes)))
+		if first then
+			print(string.rep("=", 1+_sum(colSizes)))
+		end
+		first = false
+	end
+	print(string.rep("=", 1+_sum(colSizes)))
+end
+
+return {
+	tabular_printing = tabular_printing,
+}
