40c55573f3930d752bc6fae9acafdba466f197fb
luk <mail-login+gitlab@protonmail.com>
2021-11-14 22:16:45 +0100

    implement read with lpeg

--- read.lua
+++ read.lua
@@ -1,21 +1,51 @@
+local expression = require("cond_expression")
+local utils = require("cond_expression_utils")
+local lpeg = require("lpeg")
 
-local file = io.open("t")
-local hdr = file:read("*line")
+local _M = {}
 
--- str shall be the string representation of the expression
--- expr is the table-tree representation of the expression
+local field = lpeg.S(" ")^0 * '"' * lpeg.Cs(((lpeg.P(1) - '"') + lpeg.P'""' / '"')^0) * '"' * lpeg.S(" ")^0 +
+                    lpeg.C((1 - lpeg.S'|\n"')^0)
 
--- ones  is a list of strings e.g. "010" giving 1 on the output TODO maybe string -> list
--- zeros is a list of strings e.g. "010" giving 0 on the output TODO maybe string -> list
--- => together this is table
+local record = field * ('|' * field)^0 * (lpeg.P'\n' + -1)
 
--- vars is a list of strings containing the used variables
-
--- local expression = {str=nil, expr=nil, table=nil, vars=nil, parser=G} -- default values here
-
-local exprs = {}
-local _,start = hdr:find("|  |")
-for x in hdr:gmatch([[| "(.-)" ]], start) do
-	table.insert(exprs, {str=x, ones={}, zeros={}, out={}, vars={}})
-	print(x)
+local function csv (s)
+  return lpeg.match(record, s)
 end
+
+function _M.read(fn)
+	local file = io.open(fn)
+	local hdr = file:read("*line")
+
+	local exprs = {}
+	local exprsL = {}
+	local varsB = true
+	local vars = {}
+	for _,x in ipairs(table.pack(csv(hdr))) do
+		if x == "" then varsB = false
+		elseif varsB then
+			table.insert(vars, x)
+		else
+			table.insert(exprsL, x)
+			exprs[x] = expression:new{table={}, vars=utils.shallow_copy(vars)}
+		end
+	end
+
+	local varsS,i
+	for line in file:lines() do
+		varsS, varsB, i = "", true, 1
+		for _,x in ipairs(table.pack(csv(line))) do
+			x = x:match([[%s*"?(.*)"?%s*]]) -- strip whitespaces and surrounding quotes
+			if x == "" then varsB = false
+			elseif varsB then
+				varsS = varsS .. x
+			else
+				exprs[exprsL[i]].table[varsS] = x
+				i = i+1
+			end
+		end
+	end
+	return exprs
+end
+
+return _M
