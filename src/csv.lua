07190e4cd2f57297b9140f4c9cba9b19bd2b0a56
luk <mail-login+gitlab@protonmail.com>
2021-11-16 23:54:54 +0100

    Add some tests and add fixes
    Add fixed for the troubles arose while testing.
    - Add file option to tabular_print (optional) to be even able to test it
      most probably nessacary for some `utils` and `cond` functions/tests
      too
    - csv removes trailing spaces (spaces should be matched too) without
      quotes

--- src/csv.lua
+++ src/csv.lua
@@ -2,13 +2,18 @@ local lpeg = require("lpeg")
 
 local field,record
 
+local function stripSpace(s)
+	local r = s:gsub("%s*$", "")
+	return r
+end
+
 local _M = {}
 setmetatable(_M, {
 	__call = function(_, sep)
 		assert(type(sep) == "string" and #sep == 1, "sep has to be a string and be of length 1 (effectively being a char)")
 
 		field = lpeg.S(" ")^0 * '"' * lpeg.Cs(((lpeg.P(1) - '"') + lpeg.P('""') / '"')^0) * '"' * lpeg.S(" ")^0 +
-					  lpeg.S(" ")^0 * lpeg.C((1 - lpeg.S(sep .. '\n"'))^0) * lpeg.S(" ")^0
+					  lpeg.S(" ")^0 * lpeg.C((1 - lpeg.S(sep .. '\n"'))^0) / stripSpace * lpeg.S(" ")^0
 		record = field * (sep * field)^0 * (lpeg.P'\n' + -1)
 		return _M
 	end
